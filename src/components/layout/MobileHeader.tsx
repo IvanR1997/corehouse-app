39091cc0528fb59a9dcb0a665f823ec760a51e53
Ivan Raonic <ivanraonic1997@gmail.com>
2026-06-02 18:21:57 +0200

    theme toggle na mobilnom

--- src/components/layout/MobileHeader.tsx
+++ src/components/layout/MobileHeader.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../../../public/logo.jpg'
+import { ThemeToggle } from '@/components/ThemeToggle'
 
 export function MobileHeader() {
   return (
-    <div className="md:hidden flex items-center justify-center py-3 mb-4 border-b border-border-subtle -mx-4 px-4">
+    <div className="md:hidden flex items-center justify-between py-3 mb-4 border-b border-border-subtle -mx-4 px-4">
       <Link href="/dashboard">
         <Image src={logo} alt="CoreHouse" className="h-10 w-auto object-contain" />
       </Link>
+      <ThemeToggle />
     </div>
   )
 }
